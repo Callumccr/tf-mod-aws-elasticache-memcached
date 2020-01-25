d52a07ba42a5f9c222e16e9114b14340441e6cbf
Gitlab <gitlab@eggplant.io>
2020-01-25 12:40:02 +0000

    Gitlab README commit

--- outputs.tf
+++ outputs.tf
@@ -3,27 +3,27 @@
 # -----------------------------------------------------------------------------
 
 output "cache_nodes" {
-  value       = formatlist("%s", aws_elasticache_cluster.memcached.*.id)
+  value       = formatlist("%s", aws_elasticache_cluster.default.*.id)
   description = "List of node objects including id, address, port and availability_zone"
 }
 
 output "configuration_endpoint" {
-  value       = formatlist("%s", aws_elasticache_cluster.memcached.*.configuration_endpoint)
+  value       = formatlist("%s", aws_elasticache_cluster.default.*.configuration_endpoint)
   description = "(Memcached only) The configuration endpoint to allow host discovery."
 }
 
 output "cache_endpoint_map" {
   value = tomap(
-    { "cms" = element(aws_elasticache_cluster.memcached.*.configuration_endpoint, 0), "session" = element(aws_elasticache_cluster.default.*.configuration_endpoint, 1) }
+    { "cms" = element(aws_elasticache_cluster.default.*.configuration_endpoint, 0), "session" = element(aws_elasticache_cluster.default.*.configuration_endpoint, 1) }
   )
 }
 
 output "security_group_id" {
   description = "The ID of the security group"
-  value       = aws_security_group.memcached.*.id
+  value       = aws_security_group.default.*.id
 }
 
 output "cluster_address" {
-  value       = formatlist("%s", aws_elasticache_cluster.memcached.*.cluster_address)
+  value       = formatlist("%s", aws_elasticache_cluster.default.*.cluster_address)
   description = "(Memcached only) The DNS name of the cache cluster without the port appended."
 }
